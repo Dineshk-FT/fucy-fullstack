2cff9be1b004f35d55a8ca83be72cd8f8d47a0ce
DineshRK11 <dineshfa7@gmail.com>
2024-08-20 22:31:37 +0530

    backend URl updated

--- frontend/src/services/baseApiService.js
+++ frontend/src/services/baseApiService.js
@@ -1,6 +1,7 @@
 export const configuration = {
   apiBaseUrl: `http://localhost:4000/`,
-  backendUrl: `http://localhost:5000/`,
+  // backendUrl: `http://localhost:5000/`,
+  backendUrl: `http://fucybackend-djfpgndsddbybbdm.eastus-01.azurewebsites.net/`,
 
   fileLimit: 10240, //10 MB
   allowedFileFormats: '.txt,.pdf,.doc,.docx,.xls,.xlsx,.png,.jpg,.jpeg,.gif,.csv',
