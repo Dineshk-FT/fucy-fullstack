947d4801f79cbb3e0ebc1e3d9945039796192b90
rushikesh-kanbargi <rushikeshk@vayavyalabs.com>
2024-12-17 16:40:30 +0530

    fix/Added cross button for gates to remove it or delete.

--- frontend/src/ui-component/CustomGates/VotingGate.jsx
+++ frontend/src/ui-component/CustomGates/VotingGate.jsx
@@ -1,11 +1,25 @@
+/*eslint-disable*/
 import React, { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { Handle, Position, useReactFlow } from 'reactflow';
 import CustomHandle from './CustomHandle';
 import { colorPickerTab } from './colorPicker';
 import AddPropertiesGate from '../Modal/AddPropertiesGate';
+import useStore from '../../Zustand/store';
+
+const selector = (state) => ({
+  nodes: state.nodes
+});
 
 export default function VotingGate(props) {
   const [open, setOpen] = useState(false);
+  const { nodes } = useStore(selector);
+  const { setNodes } = useReactFlow();
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleDeleteFromCanvas = () => {
+    setNodes((nodes) => nodes.filter((node) => node.id !== props.id));
+  };
+
   const handleopenModel = (e) => {
     e.preventDefault();
     // console.log('props', props)
@@ -16,7 +30,19 @@ export default function VotingGate(props) {
     setOpen(false);
   };
   return (
-    <div onContextMenu={handleopenModel}>
+    <div
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            handleDeleteFromCanvas();
+          }
+        }}
+        onContextMenu={handleopenModel}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{ position: 'relative', width: '100px', height: '100px' }}
+      >
       <CustomHandle type="target" position={Position.Top} style={{ top: '28px', opacity: 0 }} isConnectable={1} />
       <svg width="100px" height="100px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -31,6 +57,39 @@ export default function VotingGate(props) {
       </svg>
       <Handle type="source" position={Position.Bottom} style={{ bottom: '25px', opacity: 0 }} />
       {open && <AddPropertiesGate open={open} handleClose={handleClose} updateNode={props} />}
+      <div
+          className="delete-icon"
+          role="button"
+          tabIndex={0}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+              handleDeleteFromCanvas();
+            }
+          }}
+          onClick={(e) => {
+            e.stopPropagation();
+            handleDeleteFromCanvas();
+          }}
+          style={{
+            position: 'absolute',
+            width: '20px',
+            height: '19px',
+            top: '12px',
+            right: '0px',
+            background: '#f83e3e',
+            borderRadius: '50%',
+            fontSize: '0.8rem',
+            color: 'white',
+            cursor: 'pointer',
+            opacity: isHovered ? 1 : 0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            transition: 'opacity 0.2s ease-in-out'
+          }}
+        >
+          x
+        </div>
     </div>
   );
 }
